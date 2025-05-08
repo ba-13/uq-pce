ffd6501afd9f2860bc3580a77240b83a70cdfe33
Anshuman Barnwal <b.a.13.official@gmail.com>
2025-05-08 23:37:31 +0200

    add generate dataset

--- src/model/blackscholes.py
+++ src/model/blackscholes.py
@@ -1,35 +1,41 @@
+from enum import Enum
 import numpy as np
 from scipy import stats
+import numpy.typing as npt
 
 """Option pricing model
 Assuming current time is 0, option expires at T;
 and we need to price the option (either call or put) between 0 and T.
 To avoid arbitrage, market tends towards keeping call option price + strike price = asset price
 Assumptions:
+- r is yearly risk-free interest rate
+- sigma is yearly volatility
+- T is time in years (to keep dimensions consistent)
 - option price is zero for a zero valued asset
 - if asset price is high enough, call option price would just be price of asset minus the cost to buy the stock
 - just before maturity, call option price would be max{profit of buying asset, 0}
 More assumptions: https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model#Fundamental_hypotheses
 """
 
+
 def call_option_price(
-    s: np.float32,
-    t: np.float32,
-    T: np.float32,
-    K: np.float32,
-    sigma: np.float32,
-    r: np.float32,
-) -> np.float32:
+    s: npt.NDArray[np.float32],
+    t: npt.NDArray[np.float32],
+    T: npt.NDArray[np.float32],
+    K: npt.NDArray[np.float32],
+    sigma: npt.NDArray[np.float32],
+    r: npt.NDArray[np.float32],
+) -> npt.NDArray[np.float32]:
     """Generates call price at given underlying asset price
     at some time t
 
     Args:
-        s (np.float32): asset price
-        t (np.float32): prediction time
-        T (np.float32): maturity time
-        K (np.float32): strike price
-        sigma (np.float32): volatility, stddev of asset price
-        r (np.float32): risk-free interest rate
+        s (npt.NDArray[np.float32]): asset price
+        t (npt.NDArray[np.float32]): prediction time
+        T (npt.NDArray[np.float32]): maturity time
+        K (npt.NDArray[np.float32]): strike price
+        sigma (npt.NDArray[np.float32]): volatility, stddev of asset price
+        r (npt.NDArray[np.float32]): risk-free interest rate
     """
     tau = T - t
     den = sigma * np.sqrt(tau)
@@ -39,22 +45,22 @@ def call_option_price(
 
 
 def put_option_price(
-    s: np.float32,
-    t: np.float32,
-    T: np.float32,
-    K: np.float32,
-    sigma: np.float32,
-    r: np.float32,
-) -> np.float32:
+    s: npt.NDArray[np.float32],
+    t: npt.NDArray[np.float32],
+    T: npt.NDArray[np.float32],
+    K: npt.NDArray[np.float32],
+    sigma: npt.NDArray[np.float32],
+    r: npt.NDArray[np.float32],
+) -> npt.NDArray[np.float32]:
     """Generates put option price at given underlying asset price at some time t
 
     Args:
-        s (np.float32): asset price
-        t (np.float32): prediction time
-        T (np.float32): maturity time
-        K (np.float32): strike price
-        sigma (np.float32): volatility
-        r (np.float32): risk-free interest rate
+        s (npt.NDArray[np.float32]): asset price
+        t (npt.NDArray[np.float32]): prediction time
+        T (npt.NDArray[np.float32]): maturity time
+        K (npt.NDArray[np.float32]): strike price
+        sigma (npt.NDArray[np.float32]): volatility
+        r (npt.NDArray[np.float32]): risk-free interest rate
     """
     tau = T - t
     den = sigma * np.sqrt(tau)
--- src/model/model.py
+++ src/model/model.py
@@ -1,13 +1,20 @@
 from enum import Enum
+from typing import Callable
 import numpy as np
 import numpy.typing as npt
 from functools import partial
-from black_scholes import call_option_price, put_option_price
+from blackscholes import call_option_price, put_option_price
 from scipy.stats import qmc, norm, Normal, Uniform
+
+seed = 42
+np.random.seed(seed)
 
 
 class Distribution:
-    def sample(self, N: int) -> npt.NDArray[np.float64]:
+    def sample(self, N: int) -> npt.NDArray[np.float32]:
+        raise NotImplementedError("This is an abstract class method")
+
+    def isoprobabilistic(self, numbers: npt.NDArray[np.float32]):
         raise NotImplementedError("This is an abstract class method")
 
 
@@ -15,7 +22,7 @@ class FixedDistribution(Distribution):
     def __init__(self, value: float):
         self.value = value
 
-    def sample(self, N: int) -> npt.NDArray[np.float64]:
+    def sample(self, N: int) -> npt.NDArray[np.float32]:
         return np.full(N, self.value)
 
 
@@ -25,10 +32,10 @@ class NormalDistribution(Distribution):
         self.stddev = stddev
         self.sampler = Normal(mu=mean, sigma=stddev)
 
-    def sample(self, N: int) -> npt.NDArray[np.float64]:
+    def sample(self, N: int) -> npt.NDArray[np.float32]:
         return self.sampler.sample(N)
 
-    def isoprobabilistic(self, numbers: npt.NDArray[np.float64]):
+    def isoprobabilistic(self, numbers: npt.NDArray[np.float32]):
         return self.sampler.icdf(numbers)
 
 
@@ -38,24 +45,23 @@ class UniformDistribution(Distribution):
         self.high = high
         self.sampler = Uniform(a=low, b=high)
 
-    def sample(self, N: int) -> npt.NDArray[np.float64]:
+    def sample(self, N: int) -> npt.NDArray[np.float32]:
         return self.sampler.sample(N)
-    
-    def isoprobabilistic(self, numbers: npt.NDArray[np.float64]):
+
+    def isoprobabilistic(self, numbers: npt.NDArray[np.float32]):
         return self.sampler.icdf(numbers)
 
 
 # Parameters taken from
 # AAPL Mar-May 2025
 # US Treasury 10Y
-seed = 42
 INPUT_DISTRIBUTION = {
     "s": NormalDistribution(196, 0.6),
     "t": FixedDistribution(0),
-    "T": FixedDistribution(23),
+    "T": FixedDistribution(2),
     "K": UniformDistribution(180, 215),
     "sigma": UniformDistribution(0.2, 0.8),
-    "r": NormalDistribution(4.27, 0.2),
+    "r": NormalDistribution(0.0427, 0.002),
 }
 
 NUM_VARIABLES = 0
@@ -66,9 +72,39 @@ for input_name, input_dist in INPUT_DISTRIBUTION.items():
     else:
         NUM_VARIABLES += 1
 
-call_option_price_partial = partial(call_option_price, **fixed_arguments)
+sampler = qmc.LatinHypercube(d=NUM_VARIABLES, rng=seed)
 
+call_option_price_partial = partial(call_option_price, **fixed_arguments)
 put_option_price_partial = partial(put_option_price, **fixed_arguments)
 
-np.random.seed(seed)
-sampler = qmc.LatinHypercube(d=NUM_VARIABLES, rng=seed)
+
+def get_dataset(
+    N: int, model: Callable = call_option_price_partial
+) -> tuple[npt.NDArray[np.float32], npt.NDArray[np.float32], dict[str, int]]:
+    """Generates a dataset of call and put option prices
+
+    Args:
+        N (int): number of samples to generate
+
+    Returns:
+        npt.NDArray[np.float32]: dataset of call and put option prices
+    """
+    # Generate samples from the Latin Hypercube sampler
+    sample = sampler.random(N)
+    idx = 0
+    name_dimension_map = {}
+    # Transform the samples to the desired distributions
+    for input_name, input_dist in INPUT_DISTRIBUTION.items():
+        input_dist: Distribution
+        if isinstance(input_dist, FixedDistribution):
+            continue
+        name_dimension_map[input_name] = idx
+        sample[:, idx] = input_dist.isoprobabilistic(sample[:, idx])
+        idx += 1
+
+    # Calculate call and put option prices
+    outputs = model(
+        **{name: sample[:, idx] for name, idx in name_dimension_map.items()}
+    )
+
+    return outputs, sample, name_dimension_map
