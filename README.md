63f7e75d6e16210196c7abb2fd7828684141da75
Anshuman Barnwal <b.a.13.official@gmail.com>
2025-05-23 01:01:45 +0200

    Added report

--- README.md
+++ README.md
@@ -8,4 +8,14 @@ pip install -e .
 ```
 
 Check out and run `test.py` to see demonstration.  
-Problem statement [here](./assets/Mini_project_description.pdf)
+
+Problem statement [here](./assets/Mini_project_description.pdf).
+
+You can find all graphs related to the project within [here](./graphs/).
+
+# Files
+
+1. `calc_error.py` performs Global Error calculation and graph plotting.
+2. `relative_error.py` does relative error estimation and graph plotting.
+3. `kriging.py`, `pc_kriging.py`, `sensitivity.py` deals with UQPyLab.
+4. `UQ_project_merged.pdf` is the report of the mini-project.
--- relative_error.py
+++ relative_error.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from uq_pce.model.model import get_dataset, ModelType, NUM_VARIABLES
 from uq_pce.pce.pce import compute_psi, create_alphas
 
-np.random.seed(42)
-N = 100
+np.random.seed(40)
+N = 10000
 p = 3  # degree of truncation
 
 # calculate the original model mean and variance using Monte-Carlo
@@ -16,7 +16,7 @@ original_var = Y.var(axis=0)
 
 # 2.6.3: Fixed n, increasing p
 p = 1
-N = 100
+N = 80
 Y, nominalX, idx_attr_map = get_dataset(N, ModelType.CALL)
 ps = []
 Ps = []
@@ -36,12 +36,14 @@ while True:
     model_mean = coeff[0]
     model_var = np.sum(coeff[1:] ** 2)
     rerror_means.append(np.abs(model_mean - original_mean))
-    rerror_vars.append(np.abs(model_var - original_var))
+    rerror_vars.append(np.abs((model_var - original_var) / original_var))
     p += 1
 
 plt.figure()
-plt.plot(ps, rerror_means, "o-", label="Relative Mean error")
-plt.plot(ps, rerror_vars, "o-", label="Relative Variance error")
+plt.plot(ps, rerror_means, "o-", label=r"$\vert \hat\mu - \mu \vert$")
+plt.plot(
+    ps, rerror_vars, "o-", label=r"$\vert \frac{\hat\sigma^2}{\sigma^2} - 1 \vert$"
+)
 plt.yscale("log")
 plt.xlabel("Truncation degree p")
 plt.ylabel("Relative error")
@@ -50,7 +52,7 @@ plt.legend()
 plt.grid(True)
 
 # 2.6.3: Fixed p, increasing n
-p = 3
+p = 2
 alphas = create_alphas(NUM_VARIABLES, p)
 P = alphas.shape[0]
 Ns = range(P, 4 * P, 5)
@@ -64,11 +66,13 @@ for N in Ns:
     model_mean = coeff[0]
     model_var = np.sum(coeff[1:] ** 2)
     rerror_means.append(np.abs(model_mean - original_mean))
-    rerror_vars.append(np.abs(model_var - original_var))
+    rerror_vars.append(np.abs((model_var - original_var) / original_var))
 
 plt.figure()
-plt.plot(Ns, rerror_means, "o-", label="Relative Mean error")
-plt.plot(Ns, rerror_vars, "o-", label="Relative Variance error")
+plt.plot(Ns, rerror_means, "o-", label=r"$\vert \hat\mu - \mu \vert$")
+plt.plot(
+    Ns, rerror_vars, "o-", label=r"$\vert \frac{\hat\sigma^2}{\sigma^2} - 1 \vert$"
+)
 plt.yscale("log")
 plt.xlabel("Dataset size N")
 plt.ylabel("Relative error")
